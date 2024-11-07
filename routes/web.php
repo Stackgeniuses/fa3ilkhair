a9ad844cdb5850d964ef1f77bb7cf8f8bf5f0f18
khalidmarzou <khalidmarzoug@gmail.com>
2024-11-07 01:13:27 +0100

    header finished

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(app()->getLocale());  // Redirect root to default language
+});
+
+// Group routes by language
+Route::group(['prefix' => '{lang}', 'where' => ['lang' => 'fr|ar']], function () {
+    Route::get('/', function ($lang) {
+        App::setLocale($lang); // Set the locale based on URL
+        return view('welcome');
+    })->name('home');
+
+    Route::get('/projects', function () {
+        return view('projects');
+    })->name('projects');
+
+    Route::get('/gallery', function () {
+        return view('gallery');
+    })->name('gallery');
+
+    Route::get('/contact', function () {
+        return view('contact');
+    })->name('contact');
+
+    Route::get('/about', function () {
+        return view('about');
+    })->name('about');
 });
